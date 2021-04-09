147011b575aa2a958280001ddd7568ab31532a93
AlenaChupina <chupina.elen@gmail.com>
2021-04-08 21:04:26 -0500

    feat: add solution problem N11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,18 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let encryptedStr = '';
+  let counter = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      counter++;
+    } else {
+      encryptedStr += counter + str[i];
+      counter = 1;
+    }
+  }
+  return encryptedStr.replace(/[1]/g, '');
 }
 
 module.exports = encodeLine;
